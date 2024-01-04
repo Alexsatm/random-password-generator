be95e5decac58a7c352ca0e778ac86565a4187ab
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-04 21:20:23 +0300

    feat: add condition

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
 const symbol = '@#$^&*()_+~|}{[]></-=';
 
+const allChars = upperCase + lowerCase + number + symbol;
+
 const createPassword = () => {
     let password = '';
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    //будет продолжать добавлять один случаный символ
+    //пока длинна пароля не достигнет 12-ти
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
 }
